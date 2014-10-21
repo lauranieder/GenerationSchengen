d7cf669a45aa8f36d678ccdcae4d7880fad11313
Perrenoud Laura <laura.perrenoud@ecal.ch>
2014-10-21 15:28:58 +0200

    Canvas Setup

--- GS_v2/js/LIBMain.js
+++ GS_v2/js/LIBMain.js
@@ -38,3 +38,26 @@ LIB.prototype.draw = function() {
 // --------------- OneContent
 // --------------- At the end the main Init...
 MainInit();
+
+window.onload = function()
+{
+    var canvas = document.getElementById('mon_canvas');
+        if(!canvas)
+        {
+            alert("Impossible de récupérer le canvas");
+            return;
+        }
+
+    var context = canvas.getContext('2d');
+        if(!context)
+        {
+            alert("Impossible de récupérer le context du canvas");
+            return;
+        }
+
+
+    //C'est ici que l'on placera tout le code servant à nos dessins.
+    
+    context.fillStyle = "#FF0000";
+    context.fillRect(0,0,150,75);
+}
