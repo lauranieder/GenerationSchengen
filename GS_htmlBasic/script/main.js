7051c71f051752a26526d61c8a21aac99e862d78
Perrenoud Laura <laura.perrenoud@ecal.ch>
2014-10-23 16:23:35 +0200

    GS_htmlBasic Add Masonry Grid
    Masonry to align div element in grid
    + Append new div element

---
+++ GS_htmlBasic/script/main.js
@@ -0,0 +1,76 @@
+/////////////////
+//             //
+//   made by   //
+//             //
+// fragment.in //
+//             //
+//      !      //
+//             //
+/////////////////
+
+var texts = [
+    "Le lendemain, la tête reposée, les questions commencent à faire surface. Ces questions qui vont diriger mon travail ici. Le plus honnêtement possible, je me demande qu’est-ce donc qui m’attire, à l’instar de bien d’autres, dans cette ville ? D’où vient cette fascination quelque peu irrationnelle ? Pour ce qui me concerne, tout remonte à la chute du mur. J’y ai assisté en direct, à la télé. C’était lointain. Vu depuis Saze, un petit village du sud de la France.  Je m’en rappelle vaguement. C’était en novembre, une dizaine de jours avant mon anniversaire, j’allais sur me 12 ans.",
+    "Toujours trop jeune, les années nonante, je les ai passées en Suisse, à vivre une adolescence dorée, loin, très loin du tumulte berlinois. Pour aller chercher dans les clichés, la Love Parade de m’a jamais attirée. Je n’écoutais pas trop de musique électronique à l’époque, le monde des clubs n’était pas le mien. Je n’avais pas non plus d’amis squatters, artistes undergroud, ou musiciens déjantés. Je n’ai jamais porté un grand attachement à l’ambiance du Nord. Enfin, tout simplement, je n’avais absolument pas d’atomes crochus avec Berlin. En fait, j’y suis même venu assez tardivement. Pour rendre visite à un camarade de longue date s’y étant installé, sans rien connaître sur cette capitale. Et pourtant, en à bien y regarder, c’était tout comme si. Car mon attention n’était pas neutre, j’avais une idée préconçue sur Berlin qui accompagnait ma visite, sans que je ne m’en rende compte.",
+    "Je fais moi-même partie de ceux qu’on appelle les créatifs. Même si je me vois essentiellement comme un photographe documentaire – la créativité fait partie de mon travail, je ne suis pourtant pas ce que l’on appelle de manière parfois stéréotypée un « créatif », j’ai été envoyé par mon canton afin d’effectuer une résidence artistique à Berlin. J’y ai habité dans un quartier un peu éloigné de ces « places to be » que sont Kreuzberg, Friedrichshain, Prenzlauer Berg ou encore Mitte. Ces endroits, en perpétuel changement, ont énormément évolué depuis les années nonante.",
+    "Assez naturellement donc, j’ai été porté à rencontrer des personnes qui me ressemblent, qui étaient là pour des raisons qui nous rapprochaient. Nous avions un peu le même profil, une certaine « homogamie sociale » nous reliait. En venant à Berlin, j’avais l’intention d’interroger ceux qui avaient décidé de s’installer ici sur leurs motivations. Ma question de départ était assez vague. Elle émanait de cette intuition, guidée par le sens commun, qui veut que Berlin est une ville spéciale où des gens spéciaux viennent y chercher fortune. De ces gens-là, certes, il y en a. Mais il y a surtout énormément de gens comme moi, plutôt normaux — je dois avouer être dans une certaine normalité, même si cela ne me plait guère ; mais les gens « normaux » constituent l’écrasante majorité des êtres humains, de quoi se demander qu’est-ce qui fait les gens exceptionnels, ou même s’il existe vraiment des gens exceptionnels. ",
+    "Une catégorie particulière de migrants donc. Des gens comme moi. C’est à dire, non pas des individus en souffrance, des déracinés à la recherche de moyens de subsistance pour eux et leurs proches. Non, juste des personnes « ordinaires », avec des caractéristiques communes. Et c’est justement là que c’est intéressant. Il va sans dire que l’on retrouve aussi des migrants plus « classiques » à Berlin. Cependant, la ville attire également une population spécifique. Ceux-ci sont plutôt jeunes, d’âge ou d’esprit. Ils sont assez bien éduqués. Souvent issus de la classe moyenne. Et surtout, ils aiment l’aventure ! Car ils viennent s’installer à Berlin comme on fait un échange Erasmus, pour vivre une expérience.",
+    "Vivre dans la capitale allemande est une épreuve qui ne laisse pas indiffèrent. Chacun y a sa propre histoire, sa propre relation à la ville. Moi, je suis arrivé en plein milieu de l’hiver, le 4 janvier, après un détour par la Slovaquie pour célébrer le Nouvel An avec des amis chers à mon cœur. Et du jour au lendemain, je me suis retrouvé totalement seul, dans cette mégapole glaciale et plongée dans les ténèbres à partir de quatre heures de l’après-midi. Loin de mes proches, de mon cercle social, la solitude m’a prise par surprise. Elle m’a mis le vague à l’âme. Je me suis senti minuscule dans ces grandes rues, ces grands parcs enneigés, ces longues nuits.",
+    "Un jour de tempête, je suis allé me réfugier au « Café Cinema », dans le quartier de Mitte. J’y ai trouvé un havre cosy. Nick Cave en fond sonore, de vieilles affiches de films jaunies collées aux murs, un chocolat chaud entre mes mains. Voilà de quoi m’offrir un moment de réconfort, ici où l'espace n'a plus « (...) ses “solides épaules” pour résister “au néant” », comme l’écrit Francesco Masci1. Certainement est-ce le propre de toutes grandes villes. Mais ils flottent en plus à Berlin les spectres de l'Histoire. Un passé mouvementé qui donne le vertige. ",
+    "Assis dans ce vieux café, qui a vu déferler les époques sans broncher, les murs m’ont enseigné que je n’étais que de passage. Le Café Cinema est, semble-t-il, un des plus « vieux » établissements dans le quartier. Il aurait ouvert ses portes juste après la chute du mur, au plein cœur de Mitte. Tout a changé autour de lui, le Mitte du début des années nonante n’est plus le Mitte d’aujourd’hui. Et que dire de la période précédente, ou bien encore des années 40 ?! Voilà ce qui est si singulier à Berlin : cette succession étourdissante des époques, les plus récentes semblant faire table rase des plus anciennes. Il y avait dans les natures mortes hollandaises du XVIIe siècle tout un jeu de références à la fugacité de l’existence – la présence d’une montre dans le « Tableau de petit déjeuner avec gâteau de mûres », par exemple, peint en 1631 par Willem Claesz Heda. À Berlin, tout semble y faire allusion ! Et cela peut vous sauter à la gueule sans crier gare. Face au poids de l’Histoire, sa propre destinée se teinte soudain d’insignifiance. ",
+    "Vanessa, que j’ai interviewée, dit qu’il faut « être bien dans ses baskets » quand on vit dans cette ville. Sinon, tout cet espace et tout ce temps peuvent vous faire « flipper », affirme-t-elle encore. Oui, il faut être préparé pour venir à Berlin, et sans doute que je ne l’étais pas tout à fait. Mais, tout comme Sandrine le dit dans son interview, j’ai moi aussi aimé passer par ces moments éprouvants. Car c’est l’occasion de faire peau neuve."
+];
+
+// create <div class="item"></div>
+function getItemElement() {
+  var elem = document.createElement('div');
+  var wRand = Math.random();
+  var hRand = Math.random();
+  var widthClass = wRand > 0.92 ? 'w4' : wRand > 0.8 ? 'w3' : wRand > 0.6 ? 'w2' : '';
+  var heightClass = hRand > 0.85 ? 'h4' : hRand > 0.6 ? 'h3' : hRand > 0.35 ? 'h2' : '';
+    
+  elem.className = 'item ' + widthClass + ' ' + heightClass;
+    
+    var textfield = document.createElement("h4");
+    var textToDisplay = Math.floor((Math.random() * texts.length));
+    var text = document.createTextNode(texts[textToDisplay]);
+    textfield.appendChild(text);
+    elem.appendChild(textfield);
+  return elem;
+}
+                    
+
+
+docReady( function() {
+    var container = document.querySelector('#container');
+    var msnry = new Masonry( container, {
+      // options
+      columnWidth: 200,
+      itemSelector: '.item',
+        gutter: '.guttersizer',
+        isOriginLeft: true,
+        stamp: '.stamp'
+    });
+    
+    // layout Masonry again after all images have loaded
+    imagesLoaded( container, function() {
+      msnry.layout();
+    });
+
+    var button = document.querySelector('#append-button');
+    
+    
+    eventie.bind( button, 'click', function() {
+        // create new item elements
+        var elems = [];
+        var fragment = document.createDocumentFragment();
+        for ( var i = 0; i < 3; i++ ) {
+          var elem = getItemElement();
+          fragment.appendChild( elem );
+          elems.push( elem );
+        }
+        // append elements to container
+        container.appendChild( fragment );
+        // add and lay out newly appended elements
+        msnry.appended( elems );
+      });
+});
